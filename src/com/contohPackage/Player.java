4d67c7719d5d05fdd24e800be78d137d42156ab6
Saputra <saputra.uta50@gmail.com>
2024-06-15 00:36:25 +0800

    Package

---
+++ src/com/contohPackage/Player.java
@@ -0,0 +1,9 @@
+package com.contohPackage;
+
+public class Player {
+    private String name = "Saputra";
+    
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ src/com/player/Main.java
@@ -0,0 +1,9 @@
+package com.player;
+import com.contohPackage.Player;
+
+public class Main {
+    public static void main(String[] args) {
+     Player player = new Player();
+     System.out.println(player.getName());   
+    }
+}
